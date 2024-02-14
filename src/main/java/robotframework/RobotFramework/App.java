4882d0467949cb0c20de72b47b553447405862f7
ParthasarathyAi <parthasarathy8.3.2002@gmail.com>
2024-02-14 12:05:19 +0530

    framework

---
+++ src/main/java/robotframework/RobotFramework/App.java
@@ -0,0 +1,13 @@
+package robotframework.RobotFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
